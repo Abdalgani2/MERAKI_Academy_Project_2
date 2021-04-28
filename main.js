36ced1c562698a9f63a76386c4f8e0b6a2b0988e
Abdalgani2 <abdalgani.alqade@gmail.com>
2021-04-28 17:27:30 +0300

    display the irst item

--- main.js
+++ main.js
@@ -13,3 +13,16 @@ divToMainScren.append(ShopeHoodie);
 const ShopeShort = $("<button></button>");
 ShopeShort.addClass("mainButton");
 divToMainScren.append(ShopeShort);
+shopeShose.on("click",()=>{
+    divToMainScren.hide()
+
+    const divToshopeShose = $("<div></div>");
+    const shose1=$("<p>adidas shosesize 40-44 price:70$</p>");
+    const picturShose1=$("<img src='https://i.pinimg.com/736x/35/fc/04/35fc049f532a650f0ddd9a1bfdd3003f.jpg' >")
+    picturShose1.addClass("viewTheShose")
+    shose1.append(picturShose1)
+    divToshopeShose.append(shose1)
+    
+    
+    body.append(divToshopeShose)
+})
