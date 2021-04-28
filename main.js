81d4e11ae689e7820522df2821b7c485ff227e49
Abdalgani2 <abdalgani.alqade@gmail.com>
2021-04-29 00:22:10 +0300

    add 2 shose andoption size

--- main.js
+++ main.js
@@ -38,22 +38,63 @@ shopeShose.on("click", () => {
         console.log(val);
         divAddToCart.append(val);
     })
+    shose1.append("<br>")
     divToshopeShose.append(shose1);
-    const shose2 = $("<p>fila shose<br> size 38-42<br> price:40$<br></p>");
+    const shose2 = $("<p>fila shose<br> price:40$<br></p>");
     shose2.addClass("describeTheShose");
+    const size2=$("<select></select>");
+    size2.append('<option value=1>size 37-38</option>')
+    size2.append('<option value=2>size 39-40</option>')
+    size2.append('<option value=3>size 41-42</option>')
+    shose2.append(size2);
+    shose2.append("<br>")
     const addToStore2 = $("<button>add to cart</button>");
     shose2.append(addToStore2);
     addToStore2.on("click",()=>{
         divAddToCart.show()
 
     })
-    divToshopeShose.append(shose2);
-    const picturShose3 = $("<img src='https://pyxis.nymag.com/v1/imgs/e92/82c/0fc2191d5dd0aeac6a425a201619f9138f.2x.rsquare.w600.jpg'> ")
+    divToshopeShose.append(shose2); 
+    const picturShose3 = $("<img src='https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/374765/02/sv01/fnd/IND/fmt/png/Rebound-JOY-SoftFoam+-Shoes'> ")
     picturShose3.addClass("viewTheShose");
     divToshopeShose.append(picturShose3);
-    const picturShose4 = $("<img src='https://pyxis.nymag.com/v1/imgs/e92/82c/0fc2191d5dd0aeac6a425a201619f9138f.2x.rsquare.w600.jpg'> ")
+    const picturShose4 = $("<img src='https://www.famousfootwear.com/blob/product-images/20000/00/43/4/00434_pair_large.jpg'> ")
     picturShose4.addClass("viewTheShose");
     divToshopeShose.append(picturShose4);
+    const shose3 = $("<p>puma shose<br> price:60$<br></p>");
+    shose3.addClass("describeTheShose");
+    const size3=$("<select></select>");
+    size3.append('<option value=1>size 39-41</option>')
+    size3.append('<option value=2>size 42-44</option>')
+    size3.append('<option value=3>size 45-47</option>')
+    shose3.append(size3);
+    shose3.append("<br>")
+    const addToStore3 = $("<button>add to cart</button>");
+    shose3.append(addToStore3);
+    addToStore2.on("click",()=>{
+        divAddToCart.show()
+
+    })
+    divToshopeShose.append(shose3); 
+    const shose4 = $("<p>puma shose<br> price:50$<br></p>");
+    shose4.addClass("describeTheShose");
+    const size4=$("<select></select>");
+    size4.append('<option value=1>size 40-41</option>')
+    size4.append('<option value=2>size 42-43</option>')
+    size4.append('<option value=3>size 44-45</option>')
+    shose4.append(size4);
+    shose4.append("<br>")
+    const addToStore4 = $("<button>add to cart</button>");
+    shose4.append(addToStore4);
+    addToStore4.on("click",()=>{
+        divAddToCart.show()
+
+    })
+    divToshopeShose.append(shose4);
+
+
+    
+   
 
     body.append(divToshopeShose);
 })
